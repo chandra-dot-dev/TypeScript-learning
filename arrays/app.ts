fbc0e2d6fa80629e177d9180f91894c9d5147861
Chandra Kanth Reddy <chandrakanths0206@gmail.com>
2026-09-03 21:26:13 +0530

    Arrays

---
+++ arrays/app.ts
@@ -0,0 +1,16 @@
+// String
+
+let fruits:string[] = ["Apple","Mango","Grapes"];
+
+// number
+
+let marks:number[] = [90,98,100];
+
+//boolean
+let status:boolean[] = [true,false,true];
+
+
+//readonly array
+
+let marks1: readonly number[] = [90,59,34];
+
